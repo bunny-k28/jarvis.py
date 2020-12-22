9cec188491b2c95a6dfe98e296476ad46fc4c962
bunny-k28 <58970641+bunny-k28@users.noreply.github.com>
2020-12-22 12:43:40 +0530

    Update home automation proto.py

--- home automation proto.py
+++ home automation proto.py
@@ -53,3 +53,38 @@ if __name__ == '__main__':
         elif ('on' in query) and ('fan' in query):
             board.digital[8].write(0)
             speak('fan have been turned off')
+            
+        
+        elif ('status' in query) and ('controller' in query or 'arduino' in query):
+            try:
+                iter = pf.util.Iterator(board)
+                iter.start()
+
+                pin = board.get_pin('d:3:i')
+
+                time.sleep(1)
+                board_data = pin.read()
+
+                speak(f'the board temperature is {board_data} degree celsius')
+
+            except Exception as e:
+                speak('device is not connected')
+
+            if board_data >= 20:
+                speak('i thing you should turn n the cooling fan')
+                time.sleep(1)
+                speak('sir! should i turn on the cooling fan?')
+                query = takecommand().lower()
+                if 'yes' in query:
+                    speak('turning on the cooling fan')
+                    try:
+                        board.digital[3].write(0) # this will turn on the cooling fan
+
+                    except Exception as e:
+                        speak('not able to turn it on')
+
+                elif 'no' in query:
+                    speak('okay sir, but i think you should turn it on')
+
+            else:
+                speak('its in perfect condition')
